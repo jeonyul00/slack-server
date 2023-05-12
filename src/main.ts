901e54ce1d25c343dc7669f3396b340c312ee4a9
jeonyul <jeonyul00@gmail.com>
2023-05-12 17:35:34 +0900

    feat : hotreload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
+// index
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  await app.listen(port);
+  console.log(`---------- start server port is ${port} ----------`);
 }
+
 bootstrap();
