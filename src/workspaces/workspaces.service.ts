873b7510d3d1305aad26f0f0a342e8b29e022d31
jeonyul <jeonyul00@gmail.com>
2023-05-16 12:49:29 +0900

    feat : workspace controller

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -37,5 +37,78 @@ export class WorkspacesService {
     });
   }
 
-  async createWorkspace(name: string, url: string, id: number) {}
+  async createWorkspace(name: string, url: string, id: number) {
+    const workspace = this.workspacesRepository.create({
+      name,
+      url,
+      OwnerId: id,
+    });
+    const returned = await this.workspacesRepository.save(workspace);
+
+    const workspaceMember = new WorkspaceMembers();
+    workspaceMember.UserId = id;
+    workspaceMember.WorkspaceId = returned.id;
+    await this.workspaceMembersRepository.save(workspaceMember);
+
+    const channel = new Channels();
+    channel.name = '일반';
+    channel.WorkspaceId = returned.id;
+    const channelReturned = await this.channelsRepository.save(channel);
+
+    const channelMember = new ChannelMembers();
+    channelMember.UserId = id;
+    channelMember.ChannelId = channelReturned.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspace(url: string) {
+    return (
+      this.usersRepository
+        .createQueryBuilder('u')
+        .innerJoin('u.WorkspaceMembes', 'm')
+        .innerJoin('m.Workspace', 'w', 'w.url = :url', { url: url })
+        // .innerJoin('m.Workspace', 'w', `w.url = #${url}`); 위 코드와 같지만 이 코드는 sql 인젝션 위험
+        .getMany()
+    );
+  }
+  async createWorkspaceMembers(url: string, email: string) {
+    const workspace = await this.workspacesRepository.findOne({
+      where: { url },
+      join: {
+        alias: 'workspace',
+        innerJoinAndSelect: {
+          channels: 'workspace.Channels',
+        },
+      },
+    });
+
+    const user = await this.usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return null;
+    }
+
+    const workspaceMember = new WorkspaceMembers();
+    workspaceMember.WorkspaceId = workspace.id;
+    workspaceMember.UserId = user.id;
+    await this.workspaceMembersRepository.save(workspaceMember);
+
+    const channelMember = new ChannelMembers();
+    channelMember.ChannelId = workspace.Channels.find(
+      (value) => value.name === '일반',
+    ).id;
+
+    channelMember.UserId = user.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceMembers(url: string, id: number) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .innerJoin('user.Workspaces', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getOne();
+  }
 }
