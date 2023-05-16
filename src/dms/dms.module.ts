95f9c7dc1bf53b047fd9312545e67f47384c6e98
jeonyul <jeonyul00@gmail.com>
2023-05-16 16:01:51 +0900

    fix : 오탈자 수정

--- src/dms/dms.module.ts
+++ src/dms/dms.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DmsService } from './dms.service';
-import { DmsController } from './dms.controller';
+import { DMsService } from './dms.service';
+import { DMsController } from './dms.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DMs } from 'src/entities/DMs';
 import { Users } from 'src/entities/Users';
+import { Workspaces } from 'src/entities/Workspaces';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DMs, Users, Workspaces]), EventsModule],
-  providers: [DmsService],
-  controllers: [DmsController],
+  providers: [DMsService],
+  controllers: [DMsController],
 })
 export class DmsModule {}
